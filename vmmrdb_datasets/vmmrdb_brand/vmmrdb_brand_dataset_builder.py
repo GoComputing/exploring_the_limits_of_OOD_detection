a91f1b518b90d3b33ea5de4dae4d254b62f1a386
Carlos Pelaez Gonzalez <carlosprog1997@gmail.com>
2024-09-11 10:53:43 +0000

    Code to finetune on a custom dataset

---
+++ vmmrdb_datasets/vmmrdb_brand/vmmrdb_brand_dataset_builder.py
@@ -0,0 +1,56 @@
+"""vmmrdb_brand dataset."""
+
+import tensorflow_datasets as tfds
+import pandas as pd
+import os
+
+
+class Builder(tfds.core.GeneratorBasedBuilder):
+  """DatasetBuilder for vmmrdb_brand dataset."""
+
+  VERSION = tfds.core.Version('1.0.0')
+  RELEASE_NOTES = {
+      '1.0.0': 'Initial release.',
+  }
+  DATASET_DIR = '/mnt/homeGPU/aurrea_cpelaez/datasets/VMMRdb_splits/brand'
+
+  def _info(self) -> tfds.core.DatasetInfo:
+    """Returns the dataset metadata."""
+    # Specifies the tfds.core.DatasetInfo object
+    # Load classes names
+    classes_path = os.path.join(self.DATASET_DIR, 'classes.txt')
+    with open(classes_path, 'r') as f:
+        names = list(map(lambda line: line.strip(), f.readlines()))
+
+    # Return
+    return self.dataset_info_from_configs(
+        features=tfds.features.FeaturesDict({
+            # These are the features of your dataset
+            'image': tfds.features.Image(shape=(None, None, 3)),
+            'label': tfds.features.ClassLabel(names=names),
+        }),
+        supervised_keys=('image', 'label'),
+        homepage='https://github.com/faezetta/VMMRdb',
+        disable_shuffling=False
+    )
+
+  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+    """Returns SplitGenerators."""
+    # TODO(vmmrdb_brand): Downloads the data and defines the splits
+    # path = dl_manager.download_and_extract('https://todo-data-url')
+
+    generators = dict()
+    for split in ['train', 'val', 'test']:
+        split_path = os.path.join(self.DATASET_DIR, f'{split}.csv')
+        df = pd.read_csv(split_path, sep=',', index_col=0)
+        generators[split] = self._generate_examples(df.to_dict(orient='index'))
+
+    return generators
+
+  def _generate_examples(self, data):
+    """Yields examples."""
+    for row_id, row in data.items():
+      yield row_id, {
+          'image': row['path'],
+          'label': row['class'],
+      }
