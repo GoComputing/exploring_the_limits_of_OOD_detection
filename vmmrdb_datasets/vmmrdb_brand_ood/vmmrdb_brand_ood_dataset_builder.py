45c33a1e40f6c0345b2b55d425a5513349ec0a36
Carlos Pelaez Gonzalez <carlosprog1997@gmail.com>
2024-09-12 07:57:50 +0000

    Modified code to support our dataset

--- vmmrdb_datasets/vmmrdb_brand_ood/vmmrdb_brand_ood_dataset_builder.py
+++ vmmrdb_datasets/vmmrdb_brand_ood/vmmrdb_brand_ood_dataset_builder.py
@@ -1,4 +1,4 @@
-"""vmmrdb_brand dataset."""
+"""vmmrdb_brand_ood dataset."""
 
 import tensorflow_datasets as tfds
 import pandas as pd
@@ -6,7 +6,7 @@ import os
 
 
 class Builder(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for vmmrdb_brand dataset."""
+  """DatasetBuilder for vmmrdb_brand_ood dataset."""
 
   VERSION = tfds.core.Version('1.0.0')
   RELEASE_NOTES = {
@@ -14,20 +14,26 @@ class Builder(tfds.core.GeneratorBasedBuilder):
   }
   DATASET_DIR = '/mnt/homeGPU/aurrea_cpelaez/datasets/VMMRdb_splits/brand'
 
+  def _load_df(self):
+    split_path = os.path.join(self.DATASET_DIR, 'ood_test.csv')
+    df = pd.read_csv(split_path, sep=',', index_col=0)
+    df = df[df['is_ood'] == 1]
+    return df
+
   def _info(self) -> tfds.core.DatasetInfo:
     """Returns the dataset metadata."""
     # Specifies the tfds.core.DatasetInfo object
     # Load classes names
-    classes_path = os.path.join(self.DATASET_DIR, 'classes.txt')
-    with open(classes_path, 'r') as f:
-        names = list(map(lambda line: line.strip(), f.readlines()))
+    df = self._load_df()
+    names = list(df['class'].unique())
+    names.sort()
 
     # Return
     return self.dataset_info_from_configs(
         features=tfds.features.FeaturesDict({
             # These are the features of your dataset
             'image': tfds.features.Image(shape=(None, None, 3)),
-            'label': tfds.features.ClassLabel(names=names),
+            'label': tfds.features.ClassLabel(names=names)
         }),
         supervised_keys=('image', 'label'),
         homepage='https://github.com/faezetta/VMMRdb',
@@ -36,16 +42,12 @@ class Builder(tfds.core.GeneratorBasedBuilder):
 
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
-    # TODO(vmmrdb_brand): Downloads the data and defines the splits
+    # TODO(vmmrdb_brand_ood): Downloads the data and defines the splits
     # path = dl_manager.download_and_extract('https://todo-data-url')
 
     generators = dict()
-    for split in ['train', 'val', 'test', 'ood_test']:
-        split_path = os.path.join(self.DATASET_DIR, f'{split}.csv')
-        df = pd.read_csv(split_path, sep=',', index_col=0)
-        if split == 'ood_test':
-            df = df[df['is_ood'] == 0]
-        generators[split] = self._generate_examples(df.to_dict(orient='index'))
+    df = self._load_df()
+    generators['ood_test'] = self._generate_examples(df.to_dict(orient='index'))
 
     return generators
 
@@ -54,5 +56,5 @@ class Builder(tfds.core.GeneratorBasedBuilder):
     for row_id, row in data.items():
       yield row_id, {
           'image': row['path'],
-          'label': row['class'],
+          'label': row['class']
       }
